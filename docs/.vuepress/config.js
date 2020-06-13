6b9885368f020bead07864a558bfe78a92660190
Noriaki Machida <machida@a.com>
2020-06-13 12:57:02 +0900

    handson

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,35 +1,22 @@
 module.exports = {
-  title: 'HelloWorld',
+  title: "HelloWorld Machida",
   themeConfig: {
-    domain: 'https://xxx.ozaki25.now.sh',
     sidebar: [
       {
-        title: 'TOP',
-        children: ['/'],
+        title: "TOP",
+        children: ["/"],
       },
       {
-        title: 'Subject1',
-        children: [
-          '/subject1/page1',
-          '/subject1/page2',
-          '/subject1/page3',
-        ],
+        title: "Subject1",
+        children: ["/subject1/page1", "/subject1/page2", "/subject1/page3"],
       },
       {
-        title: 'Subject2',
-        children: [
-          '/subject2/page1',
-          '/subject2/page2',
-          '/subject2/page3',
-        ],
+        title: "Subject2",
+        children: ["/subject2/page1", "/subject2/page2", "/subject2/page3"],
       },
       {
-        title: 'Subject3',
-        children: [
-          '/subject3/page1',
-          '/subject3/page2',
-          '/subject3/page3',
-        ],
+        title: "Subject3",
+        children: ["/subject3/page1", "/subject3/page2", "/subject3/page3"],
       },
     ],
   },
@@ -37,13 +24,13 @@ module.exports = {
     lineNumbers: true,
   },
   plugins: {
-    '@vuepress/back-to-top': {},
-    '@vuepress/medium-zoom': {},
-    '@vuepress/pwa': {
+    "@vuepress/back-to-top": {},
+    "@vuepress/medium-zoom": {},
+    "@vuepress/pwa": {
       serviceWorker: true,
       updatePopup: true,
     },
-    'seo': {},
+    seo: {},
   },
-  head: [['link', { rel: 'manifest', href: '/manifest.json' }]],
+  head: [["link", { rel: "manifest", href: "/manifest.json" }]],
 };
--- docs/subject1/page1.md
+++ docs/subject1/page1.md
@@ -1,7 +1,27 @@
 # Page1
 
-## Topic1
+# 0.事前準備
 
-## Topic2
+## Dockerのインストール
+    
+- Docker実行環境はPCのOSによって大きく異なります
 
-## Topic3
+### MacOSの場合
+  - Docker Desktop for Macをインストールします
+  - [https://qiita.com/ama_keshi/items/b4c47a4aca5d48f2661c](https://qiita.com/ama_keshi/items/b4c47a4aca5d48f2661c)
+
+### Windows10 Proの場合
+  - Docker Desktop for Windowsをインストールします
+  - [https://qiita.com/fkooo/items/d2fddef9091b906675ca](https://qiita.com/fkooo/items/d2fddef9091b906675ca)
+
+### Windows10 Home, Windows7の場合
+  - Docker ToolBoxをインストールします
+  - [https://qiita.com/zeffy1014/items/dda78f4ab0449989dfe1](https://qiita.com/zeffy1014/items/dda78f4ab0449989dfe1)
+  - Docker Toolboxのインストール後、コマンドプロンプトやPowerShellでコマンドの実行が必要です
+
+```sh
+C:¥Users¥user > docker-machine --debug create -d virtualbox default
+```
+
+![vscode](/images/0-1.png)
+
